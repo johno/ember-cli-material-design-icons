e8de642c7c96b235b36d41001c01b63fb9d7572b
John Otander <johnotander@gmail.com>
2014-10-24 15:07:16 -0600

    Add vendor imports.

--- index.js
+++ index.js
@@ -2,4 +2,14 @@
 
 module.exports = {
   name: 'ember-cli-material-design-icons',
+
+  included: function(app) {
+    this._super.included(app);
+
+    this.app.import('vendor/css/material-design-iconic-font.css');
+    this.app.import('vendor/fonts/Material-Design-Iconic-Font.eot', { destDir: 'fonts' });
+    this.app.import('vendor/fonts/Material-Design-Iconic-Font.svg', { destDir: 'fonts' });
+    this.app.import('vendor/fonts/Material-Design-Iconic-Font.ttf', { destDir: 'fonts' });
+    this.app.import('vendor/fonts/Material-Design-Iconic-Font.woff', { destDir: 'fonts' });
+  }
 };
