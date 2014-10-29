0d14c753e4c5c38891d920fde7f7f6d43078ce0d
John Otander <johnotander@gmail.com>
2014-10-28 20:56:42 -0600

    Add spin/rotation functionality. Closes #3. Closes #4.

--- app/components/material-design-icon.js
+++ app/components/material-design-icon.js
@@ -4,15 +4,28 @@ export default Ember.Component.extend({
   name: '',
   size: null,
   iconClass: null,
+  spin: false,
+  spinReverse: false,
 
   iconClassDeclaration: function() {
     return [
       mdPrefix(this.get('name')),
       mdPrefix(this.get('size')),
+      getSpinningClassDeclaration(this.get('spin'), this.get('spinReverse')),
       this.get('iconClass')
     ].compact().join(' ');
-  }.property('name', 'size' ,'iconClass'),
+  }.property('name', 'size' ,'iconClass', 'spin', 'spinDirection'),
 });
+
+function getSpinningClassDeclaration(spin, spinReverse) {
+  if (spinReverse) {
+    return 'md-spin md-spin-reverse';
+  } else if (spin) {
+    return 'md-spin';
+  } else  {
+    return null;
+  }
+}
 
 function mdPrefix(className) {
   if (className) {
