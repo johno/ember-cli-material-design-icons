84c6a74dd76535979270281623acfa2d7158d436
John Otander <johnotander@gmail.com>
2015-06-08 12:25:19 -0600

    Fix tests

--- app/components/material-design-icon.js
+++ app/components/material-design-icon.js
@@ -1,3 +1,1 @@
-import materialDesignIcon from 'ember-cli-material-design-icons/components/material-design-icon';
-
-export default materialDesignIcon;
+export { default } from 'ember-cli-material-design-icons/components/material-design-icon';
