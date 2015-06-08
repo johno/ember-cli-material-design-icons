27cfbade2c0c7dc095ba143632b918a8c93154a2
John Otander <johnotander@gmail.com>
2015-06-08 10:38:56 -0600

    Upgrade ember-cli, correctly bind class name to component

--- app/components/material-design-icon.js
+++ app/components/material-design-icon.js
@@ -1,19 +1,3 @@
-import Ember from 'ember';
+import materialDesignIcon from 'ember-cli-material-design-icons/components/material-design-icon';
 
-export default Ember.Component.extend({
-  tagName: 'span',
-  name: '',
-  iconClass: null,
-
-  iconClassDeclaration: Ember.computed('name', 'iconClass', function() {
-    return `mdi mdi-${ this.get('name') } ${ this.get('iconClass') }`;
-  })
-});
-
-function mdPrefix(className) {
-  if (className) {
-    return 'mdi mdi-' + className;
-  } else {
-    return null;
-  }
-}
+export default materialDesignIcon;
